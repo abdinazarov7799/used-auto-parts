319df475049217c59a9115ca83fbf2a03918c31f
abdinazarov7799 <parolxato@gmail.com>
2023-08-11 23:24:58 +0500

    .

--- src/components/EnterLocation/EnterLoaction.js
+++ src/components/EnterLocation/EnterLoaction.js
@@ -11,6 +11,23 @@ const containerStyle = {
 
 const API_KEY = 'AIzaSyC5dKdEhkdbrG8XV0iEMDTG8jmrShJ6BjI';
 
+
+function DraggableMarker({ location, onDragEnd }) {
+    return (
+        <Marker
+            position={location}
+            draggable={true}
+            onDragEnd={(event) => {
+                const newPosition = {
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng(),
+                };
+                onDragEnd(newPosition);
+            }}
+        />
+    );
+}
+
 const EnterLoaction = () => {
     const [location, setLocation] = useState({
         lat: null,
@@ -25,21 +42,28 @@ const EnterLoaction = () => {
             });
         });
     };
-    return(
-      <>
-          <Button onClick={handleGetLocation}>Locatsiyani olish</Button>
-          {location.lat && location.lng && (
-              <LoadScript googleMapsApiKey={API_KEY}>
-                  <GoogleMap
-                      mapContainerStyle={containerStyle}
-                      center={location}
-                      zoom={10}
-                  >
-                      <Marker position={location} />
-                  </GoogleMap>
-              </LoadScript>
-          )}
-      </>
+
+    const handleDragEnd = (newPosition) => {
+        setLocation(newPosition);
+    };
+    return (
+        <div>
+            <Button onClick={handleGetLocation}>Locatsiyani olish</Button>
+            <Input placeholder="Latitude" value={location.lat} readOnly />
+            <Input placeholder="Longitude" value={location.lng} readOnly />
+
+            {location.lat && location.lng && (
+                <LoadScript googleMapsApiKey={API_KEY}>
+                    <GoogleMap
+                        mapContainerStyle={containerStyle}
+                        center={location}
+                        zoom={10}
+                    >
+                        <DraggableMarker location={location} onDragEnd={handleDragEnd} />
+                    </GoogleMap>
+                </LoadScript>
+            )}
+        </div>
     );
 }
 export default EnterLoaction;
