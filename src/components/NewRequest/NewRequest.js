3ff818a96b836b2db72ab36e19cffbff6c786943
abdinazarov7799 <parolxato@gmail.com>
2023-08-16 17:50:38 +0400

    .

--- src/components/NewRequest/NewRequest.js
+++ src/components/NewRequest/NewRequest.js
@@ -120,12 +120,7 @@ function NewRequest() {
         setNextStep(true);
     }
     function getCarDataFromVIN(vin) {
-        fetch(process.env.REACT_APP_VIN_DECODER_API + vin + '/' , {
-            method: 'GET',
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-            }
-        })
+        fetch(process.env.REACT_APP_VIN_DECODER_API + vin + '/' )
             .then(res => res.json())
             .then(data => console.log(data))
             .catch(err => console.log(err))
